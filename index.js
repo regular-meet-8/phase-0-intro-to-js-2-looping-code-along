b186bcb9569981a82aec2299c033ab263176f137
regular-meet-8 <eddiekim444@gmail.com>
2022-12-01 10:14:16 +0300

    done intro2jsloopinglab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(name, event){
+    const newArray = [];
+
+    for(let i =0; i<name.length; i++){
+        const outPut = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+        newArray.push(outPut)
+
+    }return newArray;
+} 
+    function countDown(n){
+        
+        do{
+            console.log(n)
+            n--;
+        }while(n >= 0)
+
+    }
